ad1b2ad98f7ac9f2257b51527938a577b76047f3
JungHyunKwon <kjh_3859@naver.com>
2017-12-08 15:12:41 +0900

    published

---
+++ README.md
@@ -0,0 +1,178 @@
+# responsive v1.0
+반응형 분기처리 플러그인이며 사용자가 분기를 지정하여 스크립트 처리를 쉽게 할 수 있습니다.
+
+## 호출
+윈도우 로드 이후 시점부터 쓸 수 있습니다. 이벤트는 문서준비 시점에 적어놔야 됩니다.
+````
+$.responsive({
+    range : {
+        wide : {
+            from: 9999,
+            to: 1201
+        },
+        web : {
+            from: 1200,
+            to: 801
+        },
+        tablet : {
+            from: 800,
+            to: 641
+        },
+        phone : {
+            from: 640,
+            to: 0
+        }
+    },
+    lowIERun : false,
+    lowIERange : ["web"],
+    interval : 250
+});
+
+or
+
+$.responsive();
+````
+$(window).triggerHandler("resize.responsive");가 기본으로 되어있고 콘솔에 현재상태가 표기됩니다.
+
+### 옵션
+#### range
+range객체가 없을경우 기본으로 wide(9999 ~ 1201), web(1200 ~ 801), tablet(800 ~ 641), phone(640 ~ 0)이 넣어져 있습니다.
+````
+# : {
+    from : n,
+    to : n
+}
+````
+##### \#
+* 분기의 이름이며 사용자가 작명하여 사용한다.
+* 반드시 소문자로만 작명해야 됩니다.
+  * 대문자 + 소문자 섞어서 작성시 모두 소문자로 치환됩니다.
+* 필터링 문자
+  * 뒤에 resized가 붙은문자 또는 resized
+  * 뒤에 none이 붙은문자 또는 none
+  * 뒤에 all이 붙은문자 또는 all
+  * pc
+  * mobile
+  * scrollbar
+  * ie7
+  * ie8
+  * ie9
+  * ie10
+  * ie11
+  * edge
+  * opera
+  * chrome
+  * firefox
+  * safari
+  * unknown
+
+###### from
+시작지점 입니다.
+
+###### to
+끝지점 입니다.
+
+*n*
+값은 숫자만 사용가능하며 from에 숫자가 아니면 9999가 지정되고 to가 숫자가 아니면 0이 지정됩니다.
+
+#### lowIERun
+* ie7, ie8에서 분기 실행여부 입니다.
+* 값은 boolean만 올 수 있습니다.
+* lowIERun에 값이 없거나 프로퍼티가 없거나 boolean이 아닐경우 기본값은 false입니다.
+
+#### lowIERange
+* lowIERun을 true로 지정했을때 나타나는 분기입니다.
+* range에서 지정한 분기를 몇개든 넣으면 ie7, ie8에서 지정한 분기만 보여주게 됩니다.
+* lowIERange의 값이 없거나 프로퍼티가 없을경우 기본값은 web입니다.
+* 중복으로 적은 값은 제거 됩니다.
+* 작성우선순위와 관련 있습니다.
+
+#### interval
+* 리사이즈 발생 후 몇초후에 함수를 실행할지 지정하는 변수입니다.
+* interval의 값이 없거나 프로퍼티가 없거나 숫자가 아닐경우 기본값은 250입니다.
+* 밀리세컨드 단위이며 숫자만 올 수 있습니다.
+
+### 메소드
+
+이름 | 매개변수 | 반환 | 설명
+| :--: | :---- | :-- | :-- |
+$.fn.responsive.destroy |  | boolean | 플러그인을 소멸시키는 함수 입니다.
+
+### $.fn.responsive.setting
+
+이름 | 값 | 설명
+| :-- | :- | :-- |
+hasHorizontalScrollbar | boolean | 지정한 객체에 가로스크롤바가 있는지 확인하는 변수입니다.
+hasVerticalScrollbar | boolean | 지정한 객체에 세로스크롤바가 있는지 확인하는 변수입니다.
+isRun | boolean | 플러그인이 실행됬는지 확인하는 변수입니다.
+isLowIE | boolean | 브라우저가 ie7 또는 ie8인지 확인하는 변수입니다.
+isResize | boolean | 리사이즈 중인지 확인하는 변수입니다.
+isScreenChage | boolean | 넓이 또는 높이가 변경되었는지 확인하는 변수입니다.
+isScreenHeightChange | boolean | 높이가 변경되었는지 확인하는 변수입니다.
+isScreenWidthChange | boolean | 넓이가 변경되었는지 확인하는 변수입니다.
+isScreenWidthAndHeightChange | boolean | 넓이와 높이가 변경되었는지 확인하는 변수입니다.
+lowIERun | boolean | 플러그인 옵션중 lowIERun의 값입니다.
+enter | array | 지정한 분기범위에 들어온 이름입니다.
+exit | array | 지정한 분기범위에 나간 이름입니다.
+lowIERange | array | 플러그인 옵션중 lowIERange의 값입니다.
+nowState | array | 현재상태에 대한 값입니다.
+prevState | array | 이전상태에 대한 값입니다.
+loadedHeight | number | 최초의 창의 높이입니다.
+loadedWidth | number | 최초의 창의 넓이입니다.
+windowHeight | number | 창의 높이값 입니다.
+windowWidth | number | 창의 넓이값 입니다.
+scrollbarWidth | number | 브라우저의 스크롤바 넓이에 대한 값이며 모바일에서 브라우저의 스크롤바의 넓이는 0이며 컴퓨터에서 접속한 브라우저의 스크롤바의 넓이는 브라우저마다 다릅니다.
+browser | string | 현재 접속한 브라우저가 무엇인지 확인하고 값은 ie7, ie8, ie9, ie10, ie11, edge, opera, chrome, firefox, safari, unknown값이 나옵니다. 브라우저를 찾지 못하면 unknown이라는 값이 나오게 됩니다.
+platform | string | 현재 접속한 기기가 무엇인지 확인하고 컴퓨터에서 접속하면 pc가 나오게되며 모바일 기기에서 접속하면 mobile이라는 값이 나오게 됩니다.
+
+### 상태, 우선순위
+1. all
+2. #All
+3. \#
+4. noneAll
+5. none
+6. allResized
+7. #AllResized
+8. noneAllResized
+
+### 이벤트
+이벤트는 $(window)객체에 걸어야 합니다.
+
+이름 | 값 | 설명
+| :-- | :- | :-- |
+responsive | object | $.fn.responsive.setting설명과 같습니다.
+state | string or array | 범위에 걸린 분기이름입니다.
+
+#### 기본
+````
+$(window).on("responsive", function(event) {
+    console.log(event);
+    console.log(event.responsive);
+    console.log(event.state);
+});
+````
+
+#### 필터
+````
+$(window).on("responsive:#", function(event) {
+    console.log(event);
+    console.log(event.responsive);
+    console.log(event.state);
+});
+````
+* 콜론(:)을 이어붙여 다중필터도 가능합니다.
+* #은 지정한 분기이름 입니다.
+* range객체에 적은 우선순위 대로 작성해주시면 됩니다.
+
+### 클래스
+* 브라우저 : ie7, ie8, ie9, ie10, ie11, edge, opera, chrome, firefox, safari, unknown 중에서 해당되는 브라우저로 클래스가 부여됩니다.
+  * 브라우저를 찾지 못하면 unknown이라는 클래스가 나오게 됩니다.
+* 플랫폼 : pc, mobile중에서 접속한 플랫폼으로 클래스가 부여됩니다.
+* 스크롤바 : 스크롤바 넓이가 있으면 scrollbar 클래스가 부여됩니다.
+* 분기 : 플러그인 옵션중 range에 적은 프로퍼티 이름이 클래스로 부여됩니다.
+
+### 제이쿼리 개발버전
+1.12.4
+
+### 브라우저 지원
+모든 브라우저를 지원합니다.
---
+++ js/jquery.responsive.js
@@ -0,0 +1,622 @@
+"use strict";
+
+/**
+ * @name responsive
+ * @author JungHyunKwon
+ * @since 2017-12-06
+ * @version 1.0
+ */
+try {
+	this.jQuery = this.jQuery || undefined;
+
+	/**
+	 * @name JSON psrse, stringify
+	 * @link {https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/JSON}
+	 */
+	if(!this.JSON) {
+		this.JSON = {
+			parse: function(sJSON) {
+				return eval('(' + sJSON + ')');
+			},
+			stringify: (function() {
+				var toString = Object.prototype.toString;
+				var isArray = Array.isArray || function(a) {
+					return toString.call(a) === '[object Array]';
+				};
+				var escMap = {
+					'"': '\\"',
+					'\\': '\\\\',
+					'\b': '\\b',
+					'\f': '\\f',
+					'\n': '\\n',
+					'\r': '\\r',
+					'\t': '\\t'
+				};
+				var escFunc = function(m) {
+					return escMap[m] || '\\u' + (m.charCodeAt(0) + 0x10000).toString(16).substr(1);
+				};
+				var escRE = /[\\"\u0000-\u001F\u2028\u2029]/g;
+				return function stringify(value) {
+					if (value == null) {
+						return 'null';
+					} else if (typeof value === 'number') {
+						return isFinite(value) ? value.toString() : 'null';
+					} else if (typeof value === 'boolean') {
+						return value.toString();
+					} else if (typeof value === 'object') {
+						if (typeof value.toJSON === 'function') {
+							return stringify(value.toJSON());
+						} else if (isArray(value)) {
+							var res = '[';
+							for (var i = 0; i < value.length; i++)
+								res += (i ? ', ' : '') + stringify(value[i]);
+							return res + ']';
+						} else if (toString.call(value) === '[object Object]') {
+							var tmp = [];
+							for (var k in value) {
+								if (value.hasOwnProperty(k))
+									tmp.push(stringify(k) + ': ' + stringify(value[k]));
+							}
+							return '{' + tmp.join(', ') + '}';
+						}
+					}
+					return '"' + value.toString().replace(escRE, escFunc) + '"';
+				};
+			})()
+		};
+	}
+	
+	//제이쿼리가 있는지 확인
+	if(jQuery) {
+		//$ 중첩 방지
+		(function($) {
+			var _$window = $(window),
+				_isWindowLoad = false,
+				_windowTempWidth = 0,
+				_windowTempHeight = 0,
+				_connectedState = _getConnectedState(),
+				_range = {},
+				_rangeCode = "",
+				_rangeProperty = [],
+				_timeout = 0,
+				_interval = 0;
+
+			/**
+			 * @name 가비지 컬렉션
+			 * @since 2017-12-06
+			 * @param {object} object
+			 * @return {object}
+			 */
+			function _freeObject(object) {
+				var result = {};
+
+				if(typeof object == "object" && !object.length) {
+					result = JSON.parse(JSON.stringify(object));
+				}
+
+				return result;
+			}
+
+			/**
+			 * @name 접속상태 가져오기
+			 * @since 2017-12-06
+			 * @return {object}
+			 */
+			function _getConnectedState() {
+				var userAgent = navigator.userAgent.toLowerCase(),
+					platform = navigator.platform.toLowerCase(),
+					platformCase = ["win16", "win32", "win64", "mac", "linux"],
+					result = {browser : "", platform : ""};
+
+				if(userAgent.indexOf("msie 7.0") > -1) {
+					result.browser = "ie7";
+				}else if(userAgent.indexOf("msie 8.0") > -1) {
+					result.browser = "ie8";
+				}else if(userAgent.indexOf("msie 9.0") > -1) {
+					result.browser = "ie9";
+				}else if(userAgent.indexOf("msie 10.0") > -1) {
+					result.browser = "ie10";
+				}else if(userAgent.indexOf("trident/7.0") > -1) {
+					result.browser = "ie11";
+				}else if(userAgent.indexOf("edge") > -1) {
+					result.browser = "edge";
+				}else if(userAgent.indexOf("opr") > -1) {
+					result.browser = "opera"; 
+				}else if(userAgent.indexOf("chrome") > -1) {
+					result.browser = "chrome";
+				}else if(userAgent.indexOf("firefox") > -1) {
+					result.browser = "firefox"; 
+				}else if(userAgent.indexOf("safari") > -1) {
+					result.browser = "safari";
+				}else{
+					result.browser = "unknown";
+				}
+				
+				if($.inArray(platform, platformCase) > -1) {
+					result.platform = "pc";
+				}else{
+					result.platform = "mobile";
+				}
+
+				return result;
+			}
+
+			/**
+			 * @name 배열 중복값 제거
+			 * @description name에서 중복값을 제거합니다.
+			 * @since 2017-12-06
+			 * @param {array || string} name
+			 * @return {array}
+			 */
+			function _removeDuplicate(name) {
+				var result = [];
+				
+				//문자, 숫자, 배열일 경우 배열로 만든다.
+				if(name.length) {
+					name = $.makeArray(name);
+				}else{
+					name = [];
+				}
+				
+				//result값에 값이 없으면 집어넣는다.
+				for(var i in name) {
+					if($.inArray(name[i], result) == -1) {
+						result.push(name[i]);
+					}
+				}
+
+				return result;
+			}
+
+			$(function() {
+				var _$target = $("body"),
+					_responsive = _getDefaultObject();
+
+				/**
+				 * @name 스크롤바 존재여부
+				 * @since 2017-12-06
+				 * @param {object} object
+				 * @return {object}
+				 */
+				function _hasScrollbar(object) {
+					var $this = $(object),
+						overflow = {
+							x : $this.css("overflow-x"),
+							y : $this.css("overflow-y")
+						},
+						result = {horizontal : false, vertical : false};
+					
+					//매핑한 객체의 넓이가 넘치면서 overflow:hidden이 아니거나 스크롤을 강제 지정한경우 스크롤이 있는걸로 간주
+					if((object.scrollWidth > object.clientWidth && overflow.x != "hidden") || overflow.x == "scroll") {
+						result.horizontal = true;
+					}
+					
+					//매핑한 객체의 높이가가 넘치면서 overflow:hidden이 아니거나 스크롤을 강제 지정한경우 스크롤이 있는걸로 간주
+					if((object.scrollHeight > object.clientHeight && overflow.y != "hidden") || overflow.y == "scroll") {
+						result.vertical = true;
+					}
+
+					return result;
+				}
+
+				/**
+				 * @name 스크롤바 넓이 구하기
+				 * @since 2017-12-06
+				 * @return {number}
+				 */
+				function _getScrollbarWidth() {
+					var result = 0,
+						$body = $("body"),
+						$scrollbar = $body.children("#responsive_scrollbar");
+					
+					//객체가 없을경우
+					if(!$scrollbar.length) {
+						$body.append("<div id=\"responsive_scrollbar\" style=\"visibility:hidden; overflow:scroll; position:absolute; top:-100px; left:-100px; z-index:-1; width:100px; height:100px;\">&nbsp;</div>");
+						$scrollbar = $body.children("#responsive_scrollbar");
+					}
+
+					result = $scrollbar[0].offsetWidth - $scrollbar[0].clientWidth;
+
+					return result;
+				}
+
+				/**
+				 * @name 기본옵션 객체 얻기
+				 * @since 2017-12-06
+				 * @return {object}
+				 */
+				function _getDefaultObject() {
+					var hasScrollbar = _hasScrollbar(_$target[0]),
+						scrollbarWidth = _getScrollbarWidth(),
+						windowWidth = (hasScrollbar.vertical) ? _$window.width() + scrollbarWidth : _$window.width(),
+						windowHeight = (hasScrollbar.horizontal) ? _$window.height() + scrollbarWidth : _$window.height(),
+						result = {
+							isRun : false,
+							enter : [],
+							exit : [],
+							nowState : [],
+							prevState : [],
+							lowIERange : [],
+							scrollbarWidth : scrollbarWidth,
+							windowWidth : windowWidth,
+							windowHeight : windowHeight,
+							windowLoadedWidth : windowWidth,
+							windowLoadedHeight : windowHeight,
+							browser : _connectedState.browser,
+							platform : _connectedState.platform,
+							hasVerticalScrollbar : hasScrollbar.vertical,
+							hasHorizontalScrollbar : hasScrollbar.horizontal,
+							isLowIE : _connectedState.browser == "ie7" || _connectedState.browser == "ie8",
+							lowIERun : false,
+							isScreenChange : false,
+							isScreenWidthChange : false,
+							isScreenHeightChange : false,
+							isScreenWidthAndHeightChange : false
+						};
+					
+					//스크롤바 객체제거
+					$("#responsive_scrollbar").remove();
+
+					return _freeObject(result);
+				}
+
+				/**
+				 * @name 분기 적용
+				 * @since 2017-12-06
+				 * @param {array[string] || string} state
+				 * @return {boolean}
+				 */
+				function _setState(state) {
+					var result = false,
+						setState = [],
+						nowState = [];
+
+					//중복제거
+					state = _removeDuplicate(state);
+
+					//적용시킬 상태가 있는지 확인
+					for(var i in state) {
+						if($.inArray(state[i], _responsive.nowState) == -1) {
+							setState.push(state[i]);
+						}else{
+							nowState.push(state[i]);
+						}
+					}
+					
+					//적용시킬 상태가 있으면 true
+					result = (setState.length) ? true : false;
+
+					if(result) {
+						//이전상태 클래스 제거
+						_$target.removeClass(_responsive.nowState.join(" "));
+
+						//현재상태 클래스 추가
+						_$target.addClass(state.join(" "));
+
+						//이전상태 갱신
+						_responsive.prevState = _responsive.nowState;
+
+						//현재상태 갱신
+						_responsive.nowState = state;
+
+						//함수실행
+						_callEvent(setState);
+
+						//console에 현재상태 표기
+						console.log("현재상태 : " + state.join(", "));
+					}
+
+					return result;
+				}
+
+				/**
+				 * @name 분기이벤트 실행
+				 * @since 2017-12-06
+				 * @param {array[string] || string} state
+				 * @return {array}
+				 */
+				function _callEvent(state) {
+					var event = {
+							responsive : _freeObject(_responsive)
+						};
+
+					//중복제거
+					state = _removeDuplicate(state);
+
+					//사용자 제공 객체 갱신
+					$.responsive.setting = event.responsive;
+
+					for(var i in state) {
+						//상태기입
+						event.state = state[i];
+
+						//모든 이벤트 호출
+						_$window.triggerHandler($.Event("responsive", event));
+
+						//필터 이벤트 호출
+						_$window.triggerHandler($.Event("responsive:" + state[i], event));
+					}
+					
+					//2개이상 필터 이벤트 호출
+					if(state.length > 1) {
+						event.state = state;
+						_$window.triggerHandler($.Event("responsive:" + state.join(":"), event));
+					}
+
+					return state;
+				}
+				
+				/**
+				 * @name 화면정보 입력
+				 * @since 2017-12-06
+				 * @return {object}
+				 */
+				function _setScreenInfo() {
+					//스크롤바 유무
+					var hasScrollbar = _hasScrollbar(_$target[0]);
+
+					//가로, 세로 스크롤바가 있는지 갱신
+					_responsive.hasVerticalScrollbar = hasScrollbar.vertical;
+					_responsive.hasHorizontalScrollbar = hasScrollbar.horizontal;
+
+					//스크롤바 넓이 갱신
+					_responsive.scrollbarWidth = _getScrollbarWidth();
+
+					//브라우저 스크롤바가 있을때
+					if(_responsive.scrollbarWidth) {
+						_$target.addClass("scrollbar");
+					}else{
+						_$target.removeClass("scrollbar");
+					}
+
+					//화면정보 등록
+					_responsive.windowWidth = _$window.width();
+					_responsive.windowHeight = _$window.height();
+
+					//세로 스크롤바가 있을때
+					if(_responsive.hasVerticalScrollbar) {
+						_responsive.windowWidth += _responsive.scrollbarWidth;
+					}
+
+					//가로 스크롤바가 있을때
+					if(_responsive.hasHorizontalScrollbar) {
+						_responsive.windowHeight += _responsive.scrollbarWidth;
+					}
+
+					return _responsive;
+				}
+
+				/**
+				 * @param {object} option {range : {# : {from : n || "*", to : n || "*"}}, lowIERun : boolean, lowIERange : ["#"], interval : n}
+				 * @return {object}
+				 */
+				$.responsive = function(option) {
+					//중복실행 검사 && 윈도우 로드 검사
+					if(!_responsive.isRun && _isWindowLoad) {
+						//실행등록
+						_responsive.isRun = true;
+
+						//기본값 상속
+						option = $.extend({
+							range : {
+								wide : {
+									from : 9999,
+									to : 1201
+								},
+
+								web : {
+									from : 1200,
+									to : 801
+								},
+
+								tablet : {
+									from : 800,
+									to : 641
+								},
+
+								phone : {
+									from : 640,
+									to : 0
+								}
+							},
+							lowIERun : false,
+							lowIERange : ["web"],
+							interval : 250
+						}, option);
+
+						//브라우저, 플랫폼 클래스 추가
+						_$target.addClass(_responsive.browser + " " + _responsive.platform);
+
+						//브라우저 스크롤바가 있을때
+						if(_responsive.scrollbarWidth) {
+							_$target.addClass("scrollbar");
+						}else{
+							_$target.removeClass("scrollbar");
+						}
+
+						//ie7, ie8에서 분기 실행여부
+						if(typeof option.lowIERun == "boolean") {
+							_responsive.lowIERun = option.lowIERun;
+						}else{
+							_responsive.lowIERun = false;
+						}
+						
+						//lowIERun을 true로 지정했을때 나타나는 분기
+						_responsive.lowIERange = _removeDuplicate(option.lowIERange);
+						if(!_responsive.lowIERange.length) {
+							_responsive.lowIERange = ["web"];
+						}
+
+						//리사이즈 종료간격 추가
+						if(typeof option.interval == "number") {
+							_interval = option.interval;
+						}else{
+							_interval = 0;
+						}
+
+						//초기화면 정보
+						_range = option.range;
+
+						//초기화
+						_rangeCode = "_responsive.enter = [];\n_responsive.exit = [];\n\n";
+						
+						//rangeCode작성 시작
+
+						//ie7 && ie8일때 예외처리
+						_rangeCode += "if(!_responsive.lowIERun && _responsive.isLowIE) {\n\t_responsive.enter = _responsive.lowIERange;\n}else{\n";
+
+						//range에 적은 값을 기준으로 자바스크립트 코드 생성
+						for(var i in _range) {
+							//소문자 변경
+							i = i.toLowerCase();
+							
+							//필터링
+							if(i.substr(-7) != "resized" && i.substr(-4) != "none" && i.substr(-3) != "all" && i != "mobile" && i != "pc" && i != "scrollbar" && i != "ie7" && i != "ie8" && i != "ie9" && i != "ie10" && i != "ie11" && i != "edge" && i != "whale" && i != "samsung" && i != "opera" && i != "chrome" && i != "firefox" && i != "safari" && i != "unknown") {
+								//프로퍼티명 기입
+								_rangeProperty.push(i);
+
+								//값이 없거나 문자일 경우
+								if(!_range[i].from || typeof _range[i].from != "number") {
+									_range[i].from = 9999;	
+								}
+
+								//값이 없거나 문자일 경우
+								if(!_range[i].to || typeof _range[i].to != "number") {
+									_range[i].to = 0;
+								}
+							
+								_rangeCode += "\tif(_responsive.windowWidth <= _range[\"" + i + "\"].from && _responsive.windowWidth >= _range[\"" + i + "\"].to) {\n";
+								_rangeCode += "\t\t_responsive.enter.push(\"" + i + "\");\n";
+								_rangeCode += "\t}else{\n";
+								_rangeCode += "\t\t_responsive.exit.push(\"" + i + "\");\n";
+								_rangeCode += "\t}\n\n";
+							}
+						}
+
+						_rangeCode += "}";
+
+						//rangeCode작성 끝
+
+						_$window.on("resize.responsive", function(event) {
+							//화면이 변경되었는지 확인하는 변수
+							_responsive.isScreenChange = false;
+							_responsive.isScreenWidthChange = false;
+							_responsive.isScreenHeightChange = false;
+							_responsive.isScreenWidthAndHeightChange = false;
+							
+							//화면정보 갱신
+							_setScreenInfo();
+
+							//기존의 스크린 넓이와 새로부여받은 스크린 넓이가 같은지 확인
+							if(_responsive.windowWidth != _windowTempWidth) {
+								_windowTempWidth = _responsive.windowWidth;
+								_responsive.isScreenWidthChange = true;
+							}
+							
+							//기존의 스크린 높이와 새로부여받은 스크린 높이가 같은지 확인
+							if(_responsive.windowHeight != _windowTempHeight) {
+								_windowTempHeight = _responsive.windowHeight;
+								_responsive.isScreenHeightChange = true;
+							}
+							
+							//기존 스크린 넓이와 높이가 둘다 변경되었을때
+							if(_responsive.isScreenWidthChange && _responsive.isScreenHeightChange) {
+								_responsive.isScreenWidthAndHeightChange = true;
+							}
+
+							//스크린의 넓이값 또는 세로값이 변경되었을때
+							if(_responsive.isScreenWidthChange || _responsive.isScreenHeightChange) {
+								_responsive.isScreenChange = true;
+							}
+							
+							//스크린의 넓이값이 변경되었을 때
+							if(_responsive.isScreenWidthChange) {
+								//리사이즈 확인 함수
+								_responsive.isResize = true;
+
+								//전체범위 함수 호출
+								_callEvent("all");
+
+								//범위실행
+								eval(_rangeCode);
+
+								//상태적용, 이벤트 호출
+								if(_responsive.enter.length) {
+									_callEvent((_responsive.enter.join("All, ") + "All").split(", "));
+									_setState(_responsive.enter);
+								}else{
+									_callEvent("noneAll");
+									_setState("none");
+								}
+
+								//돌던 setTimeout이 있으면 중단
+								if(_timeout) {
+									clearTimeout(_timeout);
+									_timeout = 0;
+								}
+								
+								//setTimeout 재등록
+								_timeout = setTimeout(function() {
+									//리사이즈가 종료되어 플래그 변경
+									_responsive.isResize = false;
+									
+									//화면정보 갱신
+									_setScreenInfo();
+									
+									_callEvent("allResized");
+
+									if(_responsive.enter.length) {
+										_callEvent((_responsive.enter.join("AllResized, ") + "AllResized").split(", "));
+									}else{
+										_callEvent("noneAllResized");
+									}
+								}, _interval);
+							}
+						}).triggerHandler("resize.responsive");
+
+						//사용자 제공 객체 갱신
+						$.responsive.setting = _freeObject(_responsive);
+					}else{
+						throw "이미 실행 중 이거나 윈도우가 로드되지 않았습니다.";
+					}
+					
+					//객체 반환
+					return this;
+				};
+
+				/**
+				 * @name 반응형 플러그인 소멸
+				 * @since 2017-12-06
+				 * @return {object}
+				 */
+				$.responsive.destroy = function() {
+					var result = false;
+					
+					//플러그인을 실행중일때
+					if(_responsive.isRun) {
+						_$window.off("resize.responsive");
+						_$target.removeClass("scrollbar " + _responsive.browser + " " + _responsive.platform + " " + _responsive.nowState.join(" "));
+						_responsive = _getDefaultObject();
+						_windowTempWidth = 0;
+						_windowTempHeight = 0;
+						this.setting = undefined;
+						result = true;
+					}
+
+					return result;
+				};
+
+				//사용자 객체 제공
+				$.responsive.setting = _freeObject(_responsive);
+			});
+
+			_$window.on("load.responsive", function(event) {
+				_isWindowLoad = true;
+			});
+		})(jQuery);
+	}else{
+		throw "제이쿼리가 없습니다.";
+	}
+}catch(e) {
+	console.error(e);
+}
