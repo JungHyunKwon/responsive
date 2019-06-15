a89a3f76a1a4a7afe28f890ad65c27189b107175
JungHyunKwon <kjh_3859@naver.com>
2019-06-16 01:51:29 +0900

    published

--- README.md
+++ README.md
@@ -1,202 +1,111 @@
-# responsive v1.0.0
-분기 처리 플러그인이며 사용자가 분기를 지정하여 스타일, 스크립트 처리를 쉽게 할 수 있습니다.
+# screen v1.0.0
 
 ## 호출
 ````javascript
-$.responsive({
-    range : {
-        wide : {
-            horizontal : {
-                from : 9999,
-                to : 1201
-            }
-        },
-        web : {
-            horizontal : {
-                from : 1200,
-                to : 1001
-            }
-        },
-        tablet : {
-            horizontal : {
-                from : 1000,
-                to : 641
-            }
-        },
-        phone : {
-            horizontal : {
-                from : 640,
-                to : 0
-            }
-        }
-    },
-    lowIE : {
-        property : 'web'
-    }
+$.screen({
+	state : [{
+		name : 'wide',
+		horizontal : {
+			from : 9999,
+			to : 1201
+		}
+	}, {
+		name : 'web',
+		horizontal : {
+			from : 1200,
+			to : 1001
+		}
+	}, {
+		name : 'tablet',
+		horizontal : {
+			from : 1000,
+			to : 641
+		}
+	}, {
+		name : 'phone',
+		horizontal : {
+			from : 640,
+			to : 0
+		}
+	}]
 });
 ````
 
 ### 옵션
 
-#### range
-range가 객체가 아니거나 객체 안에 속성이 없을 경우 none이 떨어집니다.
+#### state(Array)
+상태의 목록입니다.
 
-````javascript
-string : {
-    horizontal : {
-        from : number,
-        to : number
-    },
-    vertical : {
-        from : number,
-        to : number
-    }
-}
-````
-##### string
-* 분기의 이름이며 사용자가 작명하여 사용한다.
-* 분기가 객체가 아닐 경우 제외됩니다.
+##### name(String)
+상태의 이름입니다.
 
-###### 필터링
-* 콤마(,)가 있는 문자
-* 뒤에 Resized가 붙은문자
-* 뒤에 All이 붙은문자
-* all
-* none
-* pc
-* mobile
-* scrollbar
-* ie7
-* ie8
-* ie9
-* ie10
-* ie11
-* edge
-* opera
-* chrome
-* firefox
-* safari
-* unknown
-* square
-* portrait
-* landscape
+##### horizontal(Object)
+가로에 대한 범위입니다.
 
-###### horizontal
-* 가로에 대한 범위입니다.
-* horizontal이 객체가 아닐 경우 제외됩니다.
+##### vertical(Object)
+세로에 대한 범위입니다.
 
-###### vertical
-* 세로에 대한 범위입니다.
-* vertical이 객체가 아닐 경우 제외됩니다.
+###### from(Number)
+시작점입니다.
 
-*from*
-시작 지점입니다.
+###### to(Number)
+끝점입니다.
 
-*to*
-종료 지점입니다.
-
-from과 to가 숫자가 아닐 경우 horizontal 또는 vertical은 제외됩니다.
-
-*number*
-값은 숫자만 사용 가능합니다.
-
-#### lowIE
-ie7, 8에서의 옵션입니다.
-
-````javascript
-lowIE : {
-    property : array[string] || string
-}
-````
-
-##### property
-* range에서 지정한 분기를 몇 개든 넣으면 ie7, 8에서 지정한 분기만 보여주게 됩니다.
-* range에 작성한 속성 이름에서 필터링 된 이름에서 없을 경우 ie7, 8에서 분기 제한이 걸리지 않습니다. 
-* 중복으로 적은 값은 제거됩니다.
-* 작성 우선순위와 관련 있습니다.
-
-### $.responsive 메서드
+### $.screen 메서드
 
 이름 | 매개변수 | 반환 | 설명
 | :-- | :---- | :-- | :-- |
-destroy |  | boolean | 플러그인을 소멸시키는 함수입니다.
-setState | array[string] \|\| string, number | boolean \|\| string | state에 지정한 분기를 적용시킬 수 있고 일자를 지정하면 일정 기간 동안 state에 지정한 분기를 볼 수 있습니다.
-getStateCookie | | array | 쿠키에 등록된 분기를 얻을 수 있습니다.
+destroy | | | 플러그인을 소멸시키는 함수입니다.
+setState | array \|\| string | boolean | 상태를 적용할 수 있습니다.
 
-### $.responsive.settings
-
-이름 | 형태 | 설명
-| :-- | :-- | :-- |
-hasHorizontalScrollbar | boolean | 가로 스크롤바가 있는지 확인하는 변수입니다.
-hasVerticalScrollbar | boolean | 세로 스크롤바가 있는지 확인하는 변수입니다.
-isRun | boolean | 플러그인이 실행됐는지 확인하는 변수입니다.
-isScreenChange | boolean | 화면이 변경되었는지 확인하는 변수입니다.
-isScreenHeightChange | boolean | 높이가 변경되었는지 확인하는 변수입니다.
-isScreenWidthChange | boolean | 넓이가 변경되었는지 확인하는 변수입니다.
-isScreenWidthAndHeightChange | boolean | 넓이와 높이가 변경되었는지 확인하는 변수입니다.
-lowIE | object | <ul><li>is(boolean) : 브라우저가 ie7, 8 인지 확인하는 변수입니다.</li><li>property(array) : lowIE에 property에 작성한 값을 토대로 반영된 값입니다.</li><li>run(boolean) :  속성에 적은 값이 있으면서 ie7, 8에서의 동작을 확인하는 변수입니다.</li></ul>
-range | object | property(array) : 플러그인 옵션에서 range에 작성한 값을 토대로 반영된 값입니다.
-rangeProperty | array | 플러그인 옵션에서 range에 작성한 값을 토대로 반영된 속성 이름 입니다.
-exit | array | 분기 범위에서 나간 이름입니다.
-nowState | array | 현재 상태에 대한 값입니다.
-prevState | array | 이전 상태에 대한 값입니다.
-orientation | string | 가로, 세로가 같을 때 square가 나오고 세로보다 가로가 더 클 때 landscape가 나오고 가로보다 세로가 더 클 때 portrait라는 값이 나옵니다.
-loadedScreenWidth | number | 로드되었을 때 창의 넓이입니다.
-loadedScreenHeight | number | 로드되었을 때 창의 높이입니다.
-screenWidth | number | 창의 넓이입니다.
-screenHeight | number | 창의 높이입니다.
-scrollbarWidth | number | 브라우저 스크롤바 넓이에 대한 값입니다.
-browser | string | 현재 접속한 브라우저가 무엇인지 확인하고 값은 ie7, ie8, ie9, ie10, ie11, edge, opera, chrome, firefox, safari, unknown 값이 나옵니다. 브라우저를 찾지 못하면 unknown이라는 값이 나오게 됩니다.
-platform | string | 현재 접속한 기기가 무엇인지 확인하고 컴퓨터에서 접속하면 pc가 나오게 되며 그 이외로는 mobile이라는 값이 나오게 됩니다.
-triggerType | string | resize 또는 resize.responsive 이벤트를 trigger 또는 triggerHandler로 호출했을 때 어떤 메서드로 호출했는지 알 수 있는 변수입니다. trigger 또는 triggerHandler에 의한 호출이 아니거나 모든 이벤트의 호출이 끝나면 공백이 나옵니다.
-
-### 상태, 우선순위
-1. all
-2. #All
-3. \#
-4. allResized
-5. #AllResized
-
-### 이벤트
-이벤트는 $(window)에 걸어야 합니다.
+### $.screen.settings
 
 이름 | 형태 | 설명
 | :-- | :-- | :-- |
-state | string | 범위에 걸린 분기 이름입니다.
-settings | object | $.responsive.settings과 같습니다.
+hasHorizontalScrollbar | boolean | 가로 스크롤바 여부
+hasVerticalScrollbar | boolean | 세로 스크롤바 여부
+scrollbarSize | number | 스크롤바 크기
+isChangedWidth | boolean | 화면 넓이 변경 여부
+isChangedHeight | boolean | 화면 높이 변경 여부
+width | number | 화면 넓이
+height | number | 화면 높이
+state | array | 상태
+name | array | 이름
+					
+### 상태
+1. resize : 리사이즈 했을 때
+2. resize:# : 리사이즈 했을 때 + 상태
+3. \# : 상태
+4. resized  : 리사이즈가 끝났을 때
+5. resized:# : 리사이즈가 끝났을 때 + 상태
 
-걸린 시점의 값이 떨어집니다.
+위 우선순위로 출력 됩니다.
+
+### event
+
+이름 | 형태 | 설명
+| :-- | :-- | :-- |
+state | string | 상태
 
 #### 기본
 ````javascript
-$(window).on('responsive', function(event) {
+$(window).on('screen', function(event) {
      console.log(event);
      console.log(event.state);
-     console.log(event.settings);
 });
 ````
 
 #### 필터
 ````javascript
-$(window).on('responsive:#', function(event) {
+$(window).on('screen:#', function(event) {
      console.log(event);
      console.log(event.state);
-     console.log(event.settings);
 });
 ````
-* #은 range에 적은 속성이 분기 이름 입니다.
 
-### 클래스
-* 브라우저 : ie7, ie8, ie9, ie10, ie11, edge, opera, chrome, firefox, safari, unknown 중에서 해당되는 브라우저로 클래스가 부여됩니다.
-  * 브라우저를 찾지 못하면 unknown이라는 클래스가 부여됩니다.
-* 플랫폼 : pc, mobile중에서 접속한 플랫폼으로 클래스가 부여됩니다.
-* 분기 : 플러그인 옵션 중 range에 적은 속성이 클래스로 부여됩니다.
-* 스크롤바 : 스크롤바 넓이가 있으면 scrollbar라는 클래스가 부여됩니다.
-* 방향 : 가로, 세로가 같을 때 square가 나오고 세로보다 가로가 더 클 때 landscape가 나오고 가로보다 세로가 더 클 때 portrait라는 값이 부여됩니다.
+\#은 상태 입니다.
 
-html에 부여됩니다.
-
-### 제이쿼리 개발 버전
+### 제이쿼리 버전
 1.12.4
 
 ### 브라우저 지원
---
+++ js/jquery.screen.js
@@ -0,0 +1,549 @@
+/**
+ * @author JungHyunKwon
+ * @version 1.0.0
+ */
+try {
+	'use strict';
+
+	(function($) {
+		//제이쿼리가 함수일 때
+		if(typeof $ === 'function') {
+			var _$window = $(window),
+				_html = document.documentElement,
+				_htmlCss = _getCss(_html),
+				_$Event = $.Event,
+				_$extend = $.extend,
+				_$inArray = $.inArray,
+				_$isNumeric = $.isNumeric,
+				_$isArray = $.isArray,
+				_element = document.getElementById('screen'),
+				_settings = {};
+
+			//요소가 없을 때
+			if(!_element) {
+				//요소 기입
+				_element = document.createElement('div');
+
+				//요소 아이디 기입
+				_element.id = 'screen';
+			}
+
+			/**
+			 * @name css 얻기
+			 * @since 2019-01-18
+			 * @param {element} element
+			 * @return {object}
+			 */
+			function _getCss(element) {
+				var result = {};
+
+				try {
+					result = element.currentStyle || getComputedStyle(element);
+				}catch(e) {
+					//throw e;
+				}
+
+				return result;
+			}
+
+			/**
+			 * @name 배열 중복 제거
+			 * @since 2017-12-06
+			 * @param {array} value
+			 * @return {array}
+			 */
+			function _deduplicateArray(value) {
+				var result = [];
+
+				for(var i = 0, valueLength = value.length; i < valueLength; i++) {
+					var element = value[i];
+
+					//값이 없을 때
+					if(_$inArray(element, result) === -1) {
+						result.push(element);
+					}
+				}
+
+				return result;
+			}
+
+			/**
+			 * @name 배열 거르기
+			 * @since 2017-12-06
+			 * @param {array} value
+			 * @param {array} array
+			 * @return {object}
+			 */
+			function _filterArray(value, array) {
+				var truth = [],
+					untruth = [],
+					result = {
+						truth : truth,
+						untruth : untruth
+					};
+
+				for(var i = 0, valueLength = value.length; i < valueLength; i++) {
+					var element = value[i];
+
+					//값이 있을 때
+					if(_$inArray(element, array) > -1) {
+						truth.push(element);
+					}else{
+						untruth.push(element);
+					}
+				}
+
+				return result;
+			}
+
+			/**
+			 * @name 배열 정렬
+			 * @since 2017-12-06
+			 * @param {array} value
+			 * @param {array} array
+			 * @return {array}
+			 */
+			function _sortArray(value, array) {
+				var result = [];
+
+				for(var i = 0, valueLength = value.length; i < valueLength; i++) {
+					var element = value[i],
+						index = _$inArray(element, array);
+
+					//값이 있을 때
+					if(index > -1) {
+						result[index] = element;
+					}
+				}
+
+				for(var i = 0; i < result.length; i++) {
+					//속성이 없을 때
+					if(!result.hasOwnProperty(i)) {
+						result.splice(i, 1);
+						i--;
+					}
+				}
+
+				return result;
+			}
+
+			/**
+			 * @name 스크롤바 크기 구하기
+			 * @since 2017-12-06
+			 * @return {number}
+			 */
+			function _getScrollbarSize() {
+				return _element.offsetWidth - _element.clientWidth;
+			}
+
+			/**
+			 * @name 스크롤바 여부
+			 * @since 2017-12-06
+			 * @return {object}
+			 */
+			function _hasScrollbar() {
+				var overflowX = _htmlCss.overflowX,
+					overflowY = _htmlCss.overflowY;
+
+				return {
+					horizontal : _html.scrollWidth > _html.clientWidth && overflowX !== 'hidden' || overflowX === 'scroll',
+					vertical : _html.scrollHeight > _html.clientHeight && overflowY !== 'hidden' || overflowY === 'scroll'
+				};
+			}
+
+			/**
+			 * @name 상태 거르기
+			 * @since 2017-12-06
+			 * @param {array || string} value
+			 * @return {array}
+			 */
+			function _filterState(value) {
+				var result = [];
+
+				//문자일 때
+				if(typeof value === 'string') {
+					value = [value];
+				}
+
+				//배열일 때
+				if(_$isArray(value)) {
+					var name = _settings.name;
+
+					//중복 제거 후 이름 값인 것만 걸러서 이름 순으로 정렬
+					result = _sortArray(_filterArray(_deduplicateArray(value), name).truth, name);
+				}
+
+				return result;
+			}
+
+			/**
+			 * @name 화면 설정
+			 * @since 2017-12-06
+			 */
+			function _setScreen() {
+				var hasScrollbar = _hasScrollbar(),
+					hasHorizontalScrollbar = hasScrollbar.horizontal,
+					hasVerticalScrollbar = hasScrollbar.vertical,
+					scrollbarSize = _getScrollbarSize(),
+					screenWidth = _$window.width(),
+					screenHeight = _$window.height();
+
+				//세로 스크롤바가 있을 때
+				if(hasVerticalScrollbar) {
+					screenWidth += scrollbarSize;
+				}
+
+				//가로 스크롤바가 있을 때
+				if(hasHorizontalScrollbar) {
+					screenHeight += scrollbarSize;
+				}
+
+				_settings.hasHorizontalScrollbar = hasHorizontalScrollbar;
+
+				_settings.hasVerticalScrollbar = hasVerticalScrollbar;
+
+				_settings.scrollbarSize = scrollbarSize;
+
+				_settings.width = screenWidth;
+
+				_settings.height = screenHeight;
+			}
+
+			/**
+			 * @name 상태 지정
+			 * @since 2017-12-06
+			 * @param {array} value
+			 * @return {object}
+			 */
+			function _setState(value) {
+				var state = _settings.state,
+					filterState = _filterArray(value, state),
+					activeState = filterState.untruth,
+					deactiveState = _filterArray(state, filterState.truth).untruth;
+
+				//활성화 상태가 있거나 비활성화 상태가 있을 때
+				if(activeState.length || deactiveState.length) {
+					//상태 기입
+					_settings.state = value;
+
+					//상태 표기
+					console.log('상태 : ' + value.join(', '));
+				}
+
+				return {
+					activeState : activeState,
+					deactiveState : deactiveState
+				};
+			}
+
+			/**
+			 * @name 이벤트
+			 * @since 2017-12-06
+			 * @param {array} value
+			 */
+			function _event(value) {
+				var event = _$Event();
+
+				//설정 지정
+				_$screen.settings = _$extend(_$screen.settings, _settings);
+
+				for(var i = 0, valueLength = value.length; i < valueLength; i++) {
+					var state = value[i];
+
+					//이벤트 유형 기입
+					event.type = 'screen';
+
+					//상태 기입
+					event.state = state;
+
+					//이벤트 호출
+					_$window.triggerHandler(event);
+
+					//필터 이벤트 유형 기입
+					event.type += ':' + state;
+
+					//필터 이벤트 호출
+					_$window.triggerHandler(event);
+				}
+			}
+
+			/**
+			 * @name 소멸
+			 * @since 2017-12-06
+			 */
+			function _destroy() {
+				//이벤트 제거
+				_$window.off('resize.screen');
+
+				//상태 초기화
+				_settings.state = [];
+
+				//설정 초기화
+				_$screen.settings = undefined;
+
+				//요소 제거
+				$(_element).remove();
+			}
+
+			/**
+			 * @name screen
+			 * @since 2017-12-06
+			 * @param {object} options {
+			 *	   state : [{
+			 *	       name : string,
+			 *		   horizontal : {
+			 *			   from : number,
+			 *			   to : number
+			 *		   },
+			 *		   vertical : {
+			 *			   from : number,
+			 *			   to : number
+			 *		   }
+			 *	   }]
+			 *  }
+			 * @return {jQuery}
+			 */
+			var _$screen = $.screen = function(options) {
+				//객체일 때
+				if(options) {
+					var state = options.state,
+						name = [],
+						width = 0,
+						height = 0,
+						timer = 0,
+						code = 'var inState = [],\n    outState = [];\n\n';
+
+					//소멸
+					_destroy();
+
+					//요소 추가
+					_html.appendChild(_element);
+
+					//배열일 때
+					if(_$isArray(state)) {
+						for(var i = 0, stateLength = state.length; i < stateLength; i++) {
+							var value = state[i];
+
+							//객체일 때
+							if(value) {
+								var stateName = value.name;
+
+								//문자일 때
+								if(typeof stateName === 'string') {
+									var horizontal = value.horizontal,
+										hasHorizontal = false,
+										vertical = value.vertical,
+										hasVertical = false;
+
+									//객체일 때
+									if(horizontal) {
+										var horizontalFrom = horizontal.from,
+											horizontalTo = horizontal.to;
+
+										//숫자가 아닐 때
+										if(!_$isNumeric(horizontalFrom)) {
+											horizontalFrom = -1;
+										}
+
+										//숫자가 아닐 때
+										if(!_$isNumeric(horizontalTo)) {
+											horizontalTo = -1;
+										}
+
+										//0 이상이면서 from이 to 이상으로 클 때
+										if(horizontalFrom >= 0 && horizontalTo >= 0 && horizontalFrom >= horizontalTo) {
+											hasHorizontal = true;
+										}
+									}
+
+									if(vertical) {
+										var verticalFrom = vertical.from,
+											verticalTo = vertical.to;
+
+										//숫자가 아닐 때
+										if(!_$isNumeric(verticalFrom)) {
+											verticalFrom = -1;
+										}
+
+										//숫자가 아닐 때
+										if(!_$isNumeric(verticalTo)) {
+											verticalTo = -1;
+										}
+
+										if(verticalFrom >= 0 && verticalTo >= 0 && verticalFrom >= verticalTo) {
+											hasVertical = true;
+										}
+									}
+
+									if(hasHorizontal || hasVertical) {
+										code += 'if(';
+
+										if(hasHorizontal) {
+											code += 'width <= ' + horizontalFrom + ' && width >= ' + horizontalTo;
+										}
+
+										if(hasVertical) {
+											if(hasHorizontal) {
+												code += ' && ';
+											}
+
+											code += 'height <= ' + verticalFrom + ' && height >= ' + verticalTo;
+										}
+
+										code += ') {\n';
+										code += '    inState.push(\'' + stateName + '\');\n';
+										code += '}else{\n';
+										code += '    outState.push(\'' + stateName + '\');\n';
+										code += '}\n\n';
+
+										name.push(stateName);
+									}
+								}
+							}
+						}
+					}
+
+					//이름 기입
+					_settings.name = name;
+
+					_$window.on('resize.screen', function(event) {
+						//화면 설정
+						_setScreen();
+
+						var screenWidth = _settings.width,
+							screenHeight = _settings.height,
+							resizeState = [],
+							resizedState = [],
+							isChangedWidth = false,
+							isChangedHeight = false,
+							isTrigger = (event instanceof _$Event) ? event.isTrigger : false;
+
+						//현재 화면 넓이와 새로운 넓이가 다를 때
+						if(screenWidth !== width) {
+							width = screenWidth;
+
+							isChangedWidth = true;
+						}
+
+						//현재 화면 높이와 새로운 높이가 다를 때
+						if(screenHeight !== height) {
+							height = screenHeight;
+
+							isChangedHeight = true;
+						}
+
+						//트리거일 때
+						if(isTrigger) {
+							isChangedWidth = false;
+							isChangedHeight = false;
+						}
+
+						//화면 변화 기입
+						_settings.isChangedWidth = isChangedWidth;
+						_settings.isChangedHeight = isChangedHeight;
+
+						//코드 실행
+						eval(code);
+
+						//적용시킬 상태가 없을 때
+						if(!inState.length) {
+							inState[0] = 'none';
+						}
+
+						//트리거가 아닐 때
+						if(!isTrigger) {
+							resizeState[0] = 'resize';
+							resizedState[0] = 'resized';
+						}
+
+						var setState = _setState(inState),
+							activeState = setState.activeState;
+
+						for(var i = 0, inStateLength = inState.length; i < inStateLength; i++) {
+							var value = inState[i];
+
+							//트리거가 아닐 때
+							if(!isTrigger) {
+								//리사이즈 상태 기입
+								resizeState.push('resize:' + value);
+
+								//리사이즈 종료 상태 기입
+								resizedState.push('resized:' + value);
+							}
+
+							//적용시킨 상태가 있을 때
+							if(_$inArray(value, activeState) > -1) {
+								//활성화 상태 기입
+								resizeState.push(value);
+							}
+						}
+
+						//이벤트 호출
+						_event(resizeState);
+
+						//타이머가 있을 때
+						if(timer) {
+							clearTimeout(timer);
+
+							timer = 0;
+						}
+
+						timer = setTimeout(function() {
+							//화면 설정
+							_setScreen();
+
+							//화면 변화 초기화
+							_settings.isChangedWidth = false;
+							_settings.isChangedHeight = false;
+
+							//이벤트 호출
+							_event(resizedState);
+						}, 250);
+					}).triggerHandler('resize.screen');
+				}
+
+				//요소 반환
+				return _$window;
+			};
+
+			/**
+			 * @name 소멸
+			 * @since 2017-12-06
+			 * @return {boolean}
+			 */
+			_$screen.destroy = _destroy;
+
+			/**
+			 * @name 상태 지정
+			 * @since 2017-12-06
+			 * @param {array || string} value
+			 * @return {boolean}
+			 */
+			_$screen.setState = function(value) {
+				var state = _filterState(value),
+					result = false;
+
+				//적용시킬 상태가 있을 때
+				if(state.length) {
+					var setState = _setState(state),
+						activeState = setState.activeState;
+
+					//활성화 상태가 있거나 비활성화 상태가 있을 때
+					if(activeState.length || setState.deactiveState.length) {
+						//활성화 이벤트 호출
+						_event(activeState);
+
+						//결과 기입
+						result = true;
+					}
+				}
+
+				return result;
+			};
+		}else{
+			throw '제이쿼리가 없습니다.';
+		}
+	})(window.jQuery);
+}catch(e) {
+	console.error(e);
+}
